6605b0cf249462e3873847edc29885500d47d648
Kindred <0059nai@gmail.com>
2025-10-30 18:25:27 +0300

    Initial commit

---
+++ src/main/java/com/yo/multibankingbackend/MultibankingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yo.multibankingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultibankingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultibankingBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yo/multibankingbackend/MultibankingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yo.multibankingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultibankingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
